ba39f9320b993e7524b6f81927496c283d286277
kleberchiliquinga <kachiliquingal@uce.edu.ec>
2026-01-10 15:05:22 -0500

    chore: Enable autoRefreshToken in Supabase client configuration

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,6 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true, // Save the session to localStorage
+    autoRefreshToken: true, // IMPORTANT! Renew token before it expires
+    detectSessionInUrl: true, // Helps with OAuth logins
+  },
+});
